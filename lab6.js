32994e8bb18744148bcf912ee0df2a0d2aca28be
JaCkIsO <jackisodev@gmail.com>
2021-03-28 23:38:36 -0500

    Update lab6.js
    Question 2

--- lab6.js
+++ lab6.js
@@ -9,3 +9,15 @@ function max(x, y){
 }
 // console.log(max(5,6));
 
+// Q2
+function maxOfThree(x, y, z){
+    if(x > y && x > z){
+        return x;
+    } else if (y > x && y > z){ 
+        return y;
+    } else {
+        return z;
+    }
+}
+// console.log(maxOfThree(4,7,1));
+
