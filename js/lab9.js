1e1384aec5420f489be021a69944a8635cf2e61c
JaCkIsO <jackisodev@gmail.com>
2021-04-07 00:33:10 -0500

    Update lab9.js
    Question 2 and 3

--- js/lab9.js
+++ js/lab9.js
@@ -26,3 +26,31 @@ let person = {
 let john = Object.create(person, {name : {value: "John"}, dateOfBirth : {value: new Date(1998,11,10)}})
 console.log(john.toString());
 
+// Q2
+let employee = {
+    salary: 0.0,
+    hireDate: Date.now(),
+    doJob(jobTitle){
+        console.log(`${this.name} is a ${jobTitle} who earns $${this.salary.toLocaleString()}`);
+    }
+}
+
+employee.prototype = person;
+
+let anna = Object.create(employee, {name: {value: "Anna"}, salary : { value: 249995.50} });
+anna.doJob("Programmer");
+
+// Q3
+function Person(){
+    this.name = "";
+    this.dateOfBirth = "";
+
+}
+Person.prototype.toString = function(){
+    return `{Name: ${this.name}, DateOfBirth: ${this.dateOfBirth.toISOString().substring(0,10)}}`;
+}
+let peter = new Person();
+peter.name = "Peter"
+peter.dateOfBirth = new Date(1985, 10, 10);
+console.log(peter.toString());
+
